fcf7804581fa905b54de014dada6628114dfe0a6
ThanhThat <thachthanhthat1995@gmail.com>
2023-05-09 07:42:52 +0700

    Responsive success.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+function hideMenu(navMobileElem, overlayElem) {
+  navMobileElem.style.transform = "translateX(-200%)";
+  overlayElem.style.transform = "translateX(-100%)";
+}
+
+function openMenu(navMobileElem, overlayElem) {
+  navMobileElem.style.transform = "translateX(0)";
+  overlayElem.style.transform = "translateX(0)";
+}
+
 (() => {
   const headerElem = document.querySelector(".header");
   if (!headerElem) return;
@@ -12,13 +22,15 @@
     if (e.target.closest(".menu-mobile-icon")) {
       // console.log("clicked menu mobile!")
 
-      navMobileElem.style.transform = "translateX(0)";
-      overlayElem.style.transform = "translateX(0)";
+      openMenu(navMobileElem, overlayElem);
     }
 
-    if (e.target.closest(".nav-close")) {
-      navMobileElem.style.transform = "translateX(-200%)";
-      overlayElem.style.transform = "translateX(-100%)";
+    if (
+      e.target.closest(".nav-close") ||
+      e.target.closest(".header-overlay") ||
+      e.target.closest(".nav-mobile a")
+    ) {
+      hideMenu(navMobileElem, overlayElem);
     }
   });
 })();
